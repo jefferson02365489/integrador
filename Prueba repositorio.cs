9c00c09ce87e29da7ab60cab58503d37caa36ead
jefferson02365489 <151577641+jefferson02365489@users.noreply.github.com>
2023-11-20 15:11:53 -0600

    Add files via upload

---
+++ Prueba repositorio.cs
@@ -0,0 +1,20 @@
+namespace prueba_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
